a779bc30b5039780bbe06bf19bd3c0f4f6f48e10
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 20:17:49 +0700

    tugas 27 js
    untuk niomic

---
+++ Tugas27/Tugas27.js
@@ -0,0 +1,14 @@
+function panggilString() {
+  var c = ["No","rumah saya",30, "A"];
+
+  var d = ["Jalan pangeran Tirtayasa No",70];
+
+  hasil1 = String(c);
+  console.log(hasil1);
+
+  hasil2 = String(d);
+  console.log(hasil2);
+
+}
+
+panggilString();
